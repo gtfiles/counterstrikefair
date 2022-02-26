2e98d47fe7be99fa34fccdb64939227b8c45bdb2
gtfiles <g.horvath@outlook.hu>
2022-02-26 12:35:17 +0100

    first commit

--- R/scraper.R
+++ R/scraper.R
@@ -1,61 +1,31 @@
 library(rvest)
 
-date_begin <- Sys.Date()-365
-date_end <- Sys.Date()
+date_begin <- as.character(Sys.Date()-365)
+date_end <- as.character(Sys.Date())   
 
-url_players <- paste0(
-  "https://www.hltv.org/stats/players?startDate=",
-                      date_begin,"&endDate=", date_end,"&rankingFilter=All")
-url_pistolrounds <- paste0(
-  "https://www.hltv.org/stats/players/pistols?startDate=",
-                           date_begin,"&endDate=", date_end,"&rankingFilter=All")
-url_opening <- paste0(
-  "https://www.hltv.org/stats/players/openingkills?startDate=",
-                      date_begin,"&endDate=", date_end,"&rankingFilter=All")
+playersurl <- paste0("https://www.hltv.org/stats/players?startDate=", date_begin,"&endDate=", date_end,"&rankingFilter=All")
+pistolurl <- paste0("https://www.hltv.org/stats/players/pistols?startDate=", date_begin,"&endDate=", date_end,"&rankingFilter=All")
+openingurl <- paste0("https://www.hltv.org/stats/players/openingkills?startDate=", date_begin,"&endDate=", date_end,"&rankingFilter=All")
 
-df.players <- read_html(url_players)
-df.players <- df.players %>% html_nodes(
-  xpath = "/html/body/div[2]/div/div[2]/div[1]/div[2]/table/tbody") %>%
+playerratings <- read_html(playersurl)
+playerratings = playerratings %>% html_nodes(xpath = "/html/body/div[2]/div[1]/div[2]/div[1]/div/table") %>%
   html_table()
-df.players <- data.frame(df.players)
-df.players <- data.frame(df.players$X1, df.players$X3, df.players$X4,
-                         df.players$X5, df.players$X6, df.players$X7)
-colnames(df.players) <- c("Player",
-                          "Maps",
-                          "Rounds",
-                          "KDdiff",
-                          "K/D",
-                          "Rating2.0")
-write.csv(df.players, "data/players.csv")
+df.playerratings <- data.frame(playerratings)
+df.playerratings <- data.frame(c(df.playerratings[1],df.playerratings[3:7]))
+write.csv(playerratings, "data/playerratings.csv")
 
-df.pistolrounds <- read_html(url_pistolrounds)
-df.pistolrounds <- df.pistolrounds %>% html_nodes(
-  xpath = "/html/body/div[2]/div/div[2]/div[1]/div[2]/table/tbody") %>%
+pistolrounds <- read_html(pistolurl)
+pistolrounds = pistolrounds %>% html_nodes(xpath = "/html/body/div[2]/div[1]/div[2]/div[1]/div/table") %>%
   html_table()
-df.pistolrounds <- data.frame(df.pistolrounds)
-df.pistolrounds <- data.frame(df.pistolrounds$X1, df.pistolrounds$X3,
-                              df.pistolrounds$X4, df.pistolrounds$X5,
-                              df.pistolrounds$X6)
-colnames(df.pistolrounds) <- c("Player",
-                               "Maps",
-                               "KDdiff",
-                               "K/D",
-                               "Rating2.0")
-write.csv(df.pistolrounds, "data/pistols.csv")
+df.pistolrounds <- data.frame(pistolrounds)
+pistolrounds<- data.frame(c(pistolrounds[1],pistolrounds[3:6]))
+write.csv(pistolrounds, "data/pistolrounds.csv")
 
-df.opening <- read_html(url_opening)
-df.opening <- df.opening %>% html_nodes(
-  xpath = "/html/body/div[2]/div/div[2]/div[1]/div[2]/table/tbody") %>%
+openingkills <- read_html(openingurl)
+openingkills = openingkills %>% html_nodes(xpath = "/html/body/div[2]/div[1]/div[2]/div[1]/div/table") %>%
   html_table()
-df.opening <- data.frame(df.opening)
-colnames(df.opening) <- c("Player",
-                          "Maps",
-                          "Rounds",
-                          "KPR",
-                          "DPR",
-                          "Attempts",
-                          "Success",
-                          "Rating")
-write.csv(df.opening, "data/opening.csv")
+df.openingkills <- data.frame(openingkills)
+write.csv(openingkills, "data/openingkills.csv")
 
+remove(openingkills, pistolrounds, playerratings, openingurl, pistolurl, playersurl)
 
